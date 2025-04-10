09ac62ccd6f7bae9f06d9ad884937f32a915edac
Caua-amestrong <caua.silveira@escola.pr.gov.br>
2025-04-10 16:06:28 -0300

    testando commit pela IDE

--- Variaveis_e_dados.py
+++ Variaveis_e_dados.py
@@ -2,5 +2,5 @@
 nome = "alice" #variavel string
 idade =25 #variavel inteira (int)
 altura = 1.68 #variavel float
-print("Olá, meu nome é",nome,"e tenho", idade,"anos" )  gdgdfg
+print("Olá, meu nome é",nome,"e tenho", idade,"anos" )  gdgdfgtb
 print(f"Eu tenho {idade} anos e minha altura é {altura:.2f} metros.") #executando a aplicação
