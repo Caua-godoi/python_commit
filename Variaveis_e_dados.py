700451fcea6367fbc960849ca0d986c3248bb822
Caua-amestrong <caua.silveira@escola.pr.gov.br>
2025-04-10 15:54:33 -0300

    testando commit pela IDE

---
+++ Variaveis_e_dados.py
@@ -0,0 +1,6 @@
+#variaveis e dados
+nome = "alice" #variavel string
+idade =25 #variavel inteira (int)
+altura = 1.68 #variavel float
+print("Olá, meu nome é",nome,"e tenho", idade,"anos" )
+print(f"Eu tenho {idade} anos e minha altura é {altura:.2f} metros.") #executando a aplicação
