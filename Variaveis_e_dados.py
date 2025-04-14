107f08762e15c7dda2301a198ffd33ccc0da0c62
Caua-amestrong <caua.silveira@escola.pr.gov.br>
2025-04-14 10:48:54 -0300

    testando commit pela IDE

--- Variaveis_e_dados.py
+++ Variaveis_e_dados.py
@@ -12,6 +12,6 @@ idade = 40
 PI = 3.14
 resultado = "true"
 print(nome)
-print(idade)   #sem aspas por se tratar de um variavel
+print(idade)   #sem aspas por se tratar de uma variavel
 print(PI)      #e não de uma string diretamente
 print(resultado)
